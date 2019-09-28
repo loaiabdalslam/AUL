eac40fdeba98b175a4b886114c4747b67bf4fbec
Loay Abdelsalam <loay.abdelsalam@pin-offer.com>
2019-09-28 16:07:08 +0200

    read csv file from local and requested http via datalab

---
+++ data-lab/extenstion/csv.js
@@ -0,0 +1,18 @@
+const csv = require('csvtojson')
+const request = require('request')
+
+module.exports = {
+  reader: async (jsonConfig,csvConfig) => {
+    if(jsonConfig.type == 'local'){
+      const jsonArray = await csv(csvConfig).fromFile(jsonConfig.path)
+      return  jsonArray
+    }else if (jsonConfig.type == 'request') {
+      console.log(jsonConfig.path)
+      const  data = await csv(csvConfig)
+      .fromStream(request.get(jsonConfig.path))
+      return data
+    }
+
+  }
+
+}
